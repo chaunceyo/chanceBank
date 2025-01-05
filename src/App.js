39fe9d57c0dea5f581bc1ecf5fb061bc62d15a38
Chaunceyo <chaunceyogletree725@gmail.com>
2025-01-05 04:27:09 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 import './App.css';
+import Header from './pages/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       hello
-      </header>
+     <Header/>
     </div>
   );
 }
---
+++ src/pages/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+   
+    return (
+      <header className="App-header">
+        <h1>chanceBank</h1>
+      </header>
+    );
+};
+
+
+export default Header;
